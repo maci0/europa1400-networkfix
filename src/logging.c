d679743cf261305e6555acbe685b74fdc3433c7c
Marcel Wysocki <maci.stgn@gmail.com>
2025-09-07 16:01:15 +0800

    Move logging globals to header

--- src/logging.c
+++ src/logging.c
@@ -6,15 +6,11 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdarg.h>
+#include <shlwapi.h>
 
 #include "logging.h"
 
 static const DWORD kMaxLogLines = 500;  // Max lines before rollover
-
-CRITICAL_SECTION g_LogCriticalSection;
-bool g_CriticalSectionInitialized = false;
-HANDLE g_LogFile = INVALID_HANDLE_VALUE;
-UINT32 g_LogLineCount = 0;
 
 static void ResetLogFile(void) {
     if (g_LogFile != INVALID_HANDLE_VALUE) {
@@ -81,5 +77,42 @@ static const char* GetErrorDescription(int errorCode) {
 void log_winsock_error(const char *prefix, SOCKET s, int error) {
     logf("%s: %s (%d) on socket %u", prefix,
          GetErrorDescription(error), error, (unsigned)s);
+}
+
+void init_logging(HMODULE hModule) {
+    InitializeCriticalSection(&g_LogCriticalSection);
+    g_CriticalSectionInitialized = true;
+
+    wchar_t dllPath[MAX_PATH];
+    GetModuleFileNameW(hModule, dllPath, MAX_PATH);
+    PathRemoveFileSpecW(dllPath);
+    wcscat_s(dllPath, MAX_PATH, L"\\hook_log.txt");
+
+    g_LogFile = CreateFileW(
+        dllPath,
+        GENERIC_WRITE,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (g_LogFile != INVALID_HANDLE_VALUE) {
+        SetFilePointer(g_LogFile, 0, NULL, FILE_END);
+        logf("[HOOK] DLL attached to process %lu, log: %ls", GetCurrentProcessId(), dllPath);
+    }
+}
+
+void close_logging(void) {
+    if (g_LogFile != INVALID_HANDLE_VALUE) {
+        CloseHandle(g_LogFile);
+        g_LogFile = INVALID_HANDLE_VALUE;
+    }
+
+    if (g_CriticalSectionInitialized) {
+        DeleteCriticalSection(&g_LogCriticalSection);
+        g_CriticalSectionInitialized = false;
+    }
 }
 
